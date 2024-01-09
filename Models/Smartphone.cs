08c6302d2cd18598a9ced7e6a477c83246d63904
Bfini <bruna.regina.fini@gmail.com>
2024-01-09 02:32:48 -0300

    Desafio Concluido! :)

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+        private string Modelo { get; set; }
+        
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
